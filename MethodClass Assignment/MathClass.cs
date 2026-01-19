ced7576ebc91430ca4fef1b619815fb5d3b645b8
Naif A <naifissa22@gmail.com>
2026-01-18 22:43:44 -0400

    Add files via upload

---
+++ MethodClass Assignment/MathClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass_Assignment
+{
+    // Define a class named MathClass
+    class MathClass
+    {
+        // Method that takes two integers, performs math on the first, and displays the second
+        public void DoMath(int firstNumber, int secondNumber)
+        {
+            // Multiply the first number by 2
+            int result = firstNumber * 2;
+            // Display result
+            Console.WriteLine("Result of math operation on first number: " + result);
+            // Display second number
+            Console.WriteLine("Second number is: " + secondNumber); 
+        }
+    }
+}
---
+++ MethodClass Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass_Assignment
+{
+    class Program
+    {
+        // Entry point of the application
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathClass myClass = new MathClass();
+
+            // Call method with normal parameters
+            myClass.DoMath(5, 10);
+            // Call method with named parameters
+            myClass.DoMath(firstNumber: 7, secondNumber: 20);
+            // Pause console
+            Console.WriteLine("Press any key to exit..."); 
+            Console.ReadKey();
+        }
+    }
+}
